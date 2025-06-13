e40d6f798af73ffce109eceec7859d1fbfe0f3fc
Tomanta <bzednick@gmail.com>
2025-06-13 17:28:41 -0500

    Set up repl loop

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,26 @@
+import { createInterface } from 'node:readline';
+
+export function startREPL(): void {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "pokedex > ",
+    })    
+    
+    rl.prompt();
+
+    rl.on('line', (line) => {
+        const words = cleanInput(line);
+        
+        if (words.length > 0) {
+            const commandName = words[0];
+            console.log(`Your command was: ${commandName}`);
+        }
+        
+        rl.prompt();
+    });
+}
+
 export function cleanInput(input: string): string[] {
     return input
         .toLowerCase()

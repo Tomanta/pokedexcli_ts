577564eb2c6efaea33046745b0b51219fcbfbd1b
Tomanta <bzednick@gmail.com>
2025-06-17 17:31:07 -0500

    Added pokedex command

--- src/command_pokedex.ts
+++ src/command_pokedex.ts
@@ -1,5 +1,8 @@
 import { State } from "./state.js";
 
 export async function commandPokedex(state: State): Promise<void> {
-    console.log("TODO!");
+    console.log("Your Pokedex:");
+    for (const pokemon of Object.values(state.pokedex)) {
+        console.log(` - ${pokemon.name}`);
+    }
 }
